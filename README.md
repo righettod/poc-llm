20074a38101d442b0f5eddf10b73d7fe68d34db9
righettod <dominique.righetto@gmail.com>
2025-05-21 15:47:36 +0200

    WIP

--- README.md
+++ README.md
@@ -12,22 +12,50 @@
 
 ## Labs
 
-🔬A labs has been created in order to study the different issues. This one take the context of a Chat model using RAG to get information about [Data Breach Investigations Report](https://www.verizon.com/business/resources/reports/dbir/) edition 2025 from the company [Verizon](https://www.verizon.com).
+🔬A labs has been created in order to study the different issues. This one take the context of a Chat model using RAG to get information about [Data Breach Investigations Report](https://www.verizon.com/business/resources/reports/dbir/) from the company [Verizon](https://www.verizon.com).
 
 🧑‍💻The labs was developed using [IntelliJ IDEA Community Edition](https://www.jetbrains.com/idea/download/) and is maven based.
 
 📖Technology stack of the labs:
 * [Ollama](https://ollama.com/): To have a local LLM engine.
 * Ollama model [gemma3:1b](https://ollama.com/library/gemma3:1b): To have small model using only TEXT data.
-* [LangChain4j](https://docs.langchain4j.dev/): To get the more nearest possible of the LLM concepts in the implementation of the labs.
+* [LangChain4j](https://docs.langchain4j.dev/): To get the more nearest possible approach of the LLM concepts in the implementation of the labs.
 
 📍Before to start the labs, start the model via `ollama run gemma3:1b` then, once the labs is started, you can call the model via the following HTTP request:
 
 ```bash
 curl -H "Content-Type: text/plain" -d "What is the result of 1+1?" http://localhost:8080/ask
 ```
+## Potential security weaknesses identified
+
+### Malicious input
+
+🐞If the input from the caller is used to build the **[SystemMessage](https://docs.langchain4j.dev/tutorials/ai-services#systemmessage)** then it can allow to affect the response given by the LLM. 
+
+### Malicious output
+
+🐞If a malicious content was present into the data used to train the LLM or to enrich it via RAG then it is possible that such content be returned by the LLM and, then, can be triggered depending on how the app uses the response of the LLM.
+
+### Information disclosure
+
+🐞If an LLM provided by an external provider is used, with RAG enriched with private documents, then private information will be shared with the LLM provider.
+
+### Resource exhaustion
+
+![RAQ QUERY](https://docs.langchain4j.dev/assets/images/rag-retrieval-f525d2937abc08fed5cec36a7f08a4c3.png)
+
+🐞When RAG is used, during the **retrieval** phase, if the **[UserMessage](https://docs.langchain4j.dev/tutorials/ai-services/#usermessage)** is too vague then it can cause a huge query to be performed against the embedding store (**Query Embedding** step).
+
+🐞In the same way, a huge *UserMessage* will be sent to the LLM that can cause extra cost due to the number of tokens present into the *UserMessage*.
+
 
 ## References used
 
+* https://docs.langchain4j.dev/integrations/language-models/ollama
+* https://docs.langchain4j.dev/integrations/language-models/ollama#parameters
 * https://docs.langchain4j.dev/category/tutorials
+* https://docs.langchain4j.dev/tutorials/model-parameters
+* https://docs.langchain4j.dev/tutorials/rag
+* https://docs.langchain4j.dev/tutorials/tools
+* https://glaforge.dev/posts/2025/02/27/pretty-print-markdown-on-the-console/
 
--- src/main/java/eu/righettod/poc/Controller.java
+++ src/main/java/eu/righettod/poc/Controller.java
@@ -1,19 +1,32 @@
 package eu.righettod.poc;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.request.ResponseFormat;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.List;
 
 /**
  * Single controller exposing a service to chat with the LLM.
@@ -22,11 +35,13 @@ import java.time.Duration;
 public class Controller {
 
     interface Assistant {
-        @SystemMessage("You act as a instructor and you give shortest response possible.")
+        @SystemMessage("You act as a instructor and you must provide the elements or figures to prove your reply.")
         String chat(@UserMessage String message);
     }
 
     private Assistant chatAssistant;
+
+    private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @Value("${ollama.baseurl}")
     private String ollamaBaseUrl;
@@ -43,29 +58,89 @@ public class Controller {
     @Value("${chat.memory.max.entries}")
     private int chatMemoryMaxEntries;
 
-    @Value("${debug}")
-    private boolean debug;
+    @Value("${ollama.trace.exchanges}")
+    private boolean ollamaTraceExchange;
 
     @PostConstruct
     public void initializeModel() {
-        //Configure the model execution
+        logger.info("[INIT] Index PDF files from folder 'documents'...");
+        PathMatcher pdfFileMatcher = FileSystems.getDefault().getPathMatcher("glob:*.pdf");
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(Paths.get("documents"), pdfFileMatcher);
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+        logger.info("[INIT] {} files indexed.", documents.size());
+
+        logger.info("[INIT] Configure the model execution...");
         OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaBaseUrl)
                 .modelName(ollamaModel)
                 .timeout(Duration.ofSeconds(ollamaResponseTimeout))
                 .temperature(ollamaModelCreativity)
-                .logRequests(debug)
-                .logResponses(debug)
+                .logRequests(ollamaTraceExchange)
+                .logResponses(ollamaTraceExchange)
+                .responseFormat(ResponseFormat.TEXT)
                 .build();
 
-        //Configure the "memory" of the chat to keep context across several questions
+        logger.info("[INIT] Configure the 'memory' of the chat to keep context across several questions...");
         ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(chatMemoryMaxEntries);
 
-        //Configure the chat proxy
-        chatAssistant = AiServices.builder(Assistant.class).chatModel(model).chatMemory(chatMemory).build();
+        logger.info("[INIT] Configure the chat proxy...");
+        chatAssistant = AiServices.builder(Assistant.class)
+                .chatModel(model)
+                .chatMemory(chatMemory)
+                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                .build();
+
+        logger.info("[INIT] Chat proxy ready.");
     }
 
     @PostMapping(value = "/ask", produces = MediaType.TEXT_PLAIN_VALUE, consumes = MediaType.TEXT_PLAIN_VALUE)
     public String ask(@RequestBody String userMessage) {
-        return chatAssistant.chat(userMessage);
+        return markdownToConsole(chatAssistant.chat(userMessage));
+    }
+
+    /**
+     * Pretty-print Markdown on the console.<br>
+     * All credit goes to Guillaume Laforge (see the referenced link).
+     *
+     * @param md Markdown content.
+     * @return Content converted to console output.
+     * @see "https://glaforge.dev/posts/2025/02/27/pretty-print-markdown-on-the-console/"
+     */
+    private String markdownToConsole(String md) {
+        return md
+                // Bold
+                .replaceAll("\\*\\*(.*?)\\*\\*", "\u001B[1m$1\u001B[0m")
+                // Italic
+                .replaceAll("\\*(.*?)\\*", "\u001B[3m$1\u001B[0m")
+                // Underline
+                .replaceAll("__(.*?)__", "\u001B[4m$1\u001B[0m")
+                // Strikethrough
+                .replaceAll("~~(.*?)~~", "\u001B[9m$1\u001B[0m")
+                // Blockquote
+                .replaceAll("(> ?.*)",
+                        "\u001B[3m\u001B[34m\u001B[1m$1\u001B[22m\u001B[0m")
+                // Lists (bold magenta number and bullet)
+                .replaceAll("([\\d]+\\.|-|\\*) (.*)",
+                        "\u001B[35m\u001B[1m$1\u001B[22m\u001B[0m $2")
+                // Block code (black on gray)
+                .replaceAll("(?s)```(\\w+)?\\n(.*?)\\n```",
+                        "\u001B[3m\u001B[1m$1\u001B[22m\u001B[0m\n\u001B[57;107m$2\u001B[0m\n")
+                // Inline code (black on gray)
+                .replaceAll("`(.*?)`", "\u001B[57;107m$1\u001B[0m")
+                // Headers (cyan bold)
+                .replaceAll("(#{1,6}) (.*?)\n",
+                        "\u001B[36m\u001B[1m$1 $2\u001B[22m\u001B[0m\n")
+                // Headers with a single line of text followed by 2 or more equal signs
+                .replaceAll("(.*?\n={2,}\n)",
+                        "\u001B[36m\u001B[1m$1\u001B[22m\u001B[0m\n")
+                // Headers with a single line of text followed by 2 or more dashes
+                .replaceAll("(.*?\n-{2,}\n)",
+                        "\u001B[36m\u001B[1m$1\u001B[22m\u001B[0m\n")
+                // Images (blue underlined)
+                .replaceAll("!\\[(.*?)]\\((.*?)\\)",
+                        "\u001B[34m$1\u001B[0m (\u001B[34m\u001B[4m$2\u001B[0m)")
+                // Links (blue underlined)
+                .replaceAll("!?\\[(.*?)]\\((.*?)\\)",
+                        "\u001B[34m$1\u001B[0m (\u001B[34m\u001B[4m$2\u001B[0m)");
     }
 }
